afe05603aaf247df2eb8a665ef71b6b27a6432ce
Vishal Sharma <80852001+vishalsharma89@users.noreply.github.com>
2025-02-17 18:30:12 +0530

    Time: 1 ms (97.31%), Space: 45.7 MB (44.51%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count=0;
+        for(int i=0;i<flowerbed.length;i++){
+            if(flowerbed[i]==0){
+                boolean emptyLeft = (i==0) || (flowerbed[i-1]==0);
+                boolean emptyRight = (i==flowerbed.length -1)|| (flowerbed[i+1]==0);
+            
+
+            // if both plots are empty, we can plat a flower here
+            if(emptyLeft && emptyRight){
+                flowerbed[i] =1;
+                count++;
+                if(count>=n){
+                    return true;
+                }
+            }
+            }
+        }
+        return count>=n;
+    }
+}
