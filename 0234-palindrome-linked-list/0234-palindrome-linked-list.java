a6752bdd9eff786ab76bae6aeed147632ed993cc
Vishal Sharma <80852001+vishalsharma89@users.noreply.github.com>
2025-05-24 19:04:32 +0530

    Time: 4 ms (83.34%), Space: 69.5 MB (6.92%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,20 +10,57 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> st = new Stack<>();
-        ListNode temp = head;
-        while(temp!=null){
-            st.push(temp.val);
-            temp = temp.next;
+        // Stack<Integer> st = new Stack<>();
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     st.push(temp.val);
+        //     temp = temp.next;
+        // }
+        // temp =head;
+        // while(temp!=null){
+        //     if(temp.val != st.peek()){
+        //         return false;
+        //     }
+        //     temp=temp.next;
+        //     st.pop();
+        // }
+        // return true;
+        // Finding middle
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        temp =head;
-        while(temp!=null){
-            if(temp.val != st.peek()){
+        // reverse
+        ListNode newHead = reverseList(slow.next);
+
+        // Comparing
+        ListNode first = head;
+        ListNode second = newHead;
+
+        while(second!=null){
+            if(first.val!=second.val){
+                reverseList(newHead);
                 return false;
             }
-            temp=temp.next;
-            st.pop();
+            first = first.next;
+            second = second.next;
         }
+
         return true;
     }
+
+    static ListNode reverseList(ListNode head) {
+        ListNode temp=head;
+        ListNode prev = null;
+    while(temp!=null){
+        ListNode front = temp.next;
+        temp.next= prev;
+        prev = temp;
+        temp = front;
+    }
+    return prev;
+}
 }
