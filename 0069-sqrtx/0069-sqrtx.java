004c8639357eb06aaa13dde4ea70a9dc0d9322bb
Vishal Sharma <80852001+vishalsharma89@users.noreply.github.com>
2025-09-22 14:27:10 +0530

    Time: 1 ms (84.5%), Space: 41.3 MB (12.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,19 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x < 2) return x;
-
-    int low = 1, high = x, ans = 1;
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-
-        if ((long) mid * mid <= x) {
-            ans = mid;         // candidate
-            low = mid + 1;     // search right
-        } else {
-            high = mid - 1;    // search left
+        if(x<2){
+            return x;
         }
-    }
-    return ans;
+        int ans =1;
+        int low =1;
+        int high = x;
+        
+        while(low<=high){
+            int mid = low +(high-low)/2;
+            if((long)mid*mid<=x){
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
     //     if (x < 2) return x;
 
     // // int ans = 1;
